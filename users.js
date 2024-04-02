cbebe54ad2b59ada7e34082b5ac677da9a663000
Taoavatara <156911166+Taoavatara@users.noreply.github.com>
2024-04-02 21:14:18 +0700

    Add files via upload

---
+++ users.js
@@ -0,0 +1,7 @@
+var users = {
+    "admin": "admin",
+    "jetsun": "jetsun123",
+    "GraciaCalista14": "gracia123",
+    "Taoavatara24": "liliantao"
+    // Tambahkan username dan password di sini
+};
